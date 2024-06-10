48d1478d431c8ca7129eb4cbbd63449bb3e67529
Benjamin Lee <sfw7sg@virginia.edu>
2024-06-09 21:24:25 -0400

    Fix
    Fixing little details on the different pages

--- ScoutWebsite/viewScout.js
+++ ScoutWebsite/viewScout.js
@@ -2,20 +2,22 @@
 // Edit the code so that it will parse the text and check for both first and last name at the same time 
 
 function searchScout() {
-    var input, filter, table, tr, td, i, txtValue;
+    var input, filter, table, tr, td, i, j, txtValue;
     input = document.getElementById("searchInput");
     filter = input.value.toUpperCase();
     table = document.querySelector(".scout-info table");
     tr = table.getElementsByTagName("tr");
 
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0]; 
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
+    for (i = 1; i < tr.length; i++) {
+        tr[i].style.display = "none"; // Hide all rows initially
+        for (j = 0; j < 3; j++) { // Check only the first name, last name, and rank columns
+            td = tr[i].getElementsByTagName("td")[j];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = ""; // Show the row if a match is found
+                    break;
+                }
             }
         }
     }
