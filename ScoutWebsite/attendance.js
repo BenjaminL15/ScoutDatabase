035cf0172020646145bef86d7f609e5a662c6d87
Benjamin Lee <sfw7sg@virginia.edu>
2024-06-09 21:39:36 -0400

    Attendance
    Included the attendance search in order to search through database (needs the database access)

--- ScoutWebsite/attendance.js
+++
@@ -1,8 +0,0 @@
-function toggleDatabaseView() {
-    var dbView = document.getElementById('databaseView');
-    if (dbView.style.display === 'none') {
-        dbView.style.display = 'block';
-    } else {
-        dbView.style.display = 'none';
-    }
-}
