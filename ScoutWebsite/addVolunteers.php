8a9c66dfc6aae74b5de70c287fc06666420dcb78
HusnainChoudhry <143745002+HusnainChoudhry@users.noreply.github.com>
2024-06-13 04:18:46 -0400

    Created addvolunteers function, need to debug

--- ScoutWebsite/addVolunteers.php
+++ ScoutWebsite/addVolunteers.php
@@ -1,3 +1,22 @@
+<?php
+$db = new SQLite3('DatabaseCreator.db');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+{
+    $volfirst = $_POST['vol_first'];
+    $vollast = $_POST['vol_last'];
+    $volphone = $_POST['vol_phone'];
+    $voladdress = $_POST['vol_address'];
+    $volstate = $_POST['vol_state'];
+    $volzip = $_POST['vol_zip'];
+
+    $db->exec("INSERT INTO ADULT_VOLUNTEERS(AVFNAME, AVLNAME, AVPHONE, AVADDRESS, AVSTATE, AVZIP) VALUES ($volfirst, $vollast, $volphone, $voladdress, $volstate, $volzip)");
+    $volID = $db->lastInsertRowID();
+    echo json_encode(['success' => true]);
+    exit();
+}
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,35 +36,35 @@
                     <div class="fields">
                         <div class="input-field">
                             <label>First Name</label>
-                            <input type="text" placeholder="First name" required>
+                            <input type="text" placeholder="First name" name = "vol_first" required>
                         </div>
                         <div class="input-field">
                             <label>Last Name</label>
-                            <input type="text" placeholder="Last name" required>
+                            <input type="text" placeholder="Last name" name = "vol_last" required>
                         </div>
                         <div class="input-field">
                             <label>Phone Number</label>
-                            <input type="tel" placeholder="Phone Number" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" required>
+                            <input type="tel" placeholder="Phone Number" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" name = "vol_phone" required>
                         </div>
                         <div class="input-field">
                             <label>Address</label>
-                            <input type="text" placeholder="Address" required>
+                            <input type="text" placeholder="Address" name = "vol_address" required>
                         </div>
                         <div class="input-field">
                             <label>State</label>
-                            <input type="text" placeholder="State" required>
+                            <input type="text" placeholder="State" name = "vol_state" required>
                         </div>
                         <div class="input-field">
                             <label>Zip Code</label>
-                            <input type="text" placeholder="Zip" required>
+                            <input type="text" placeholder="Zip" name = "vol_zip" required>
                         </div>
                         <div class="input-field">
                             <label for="parent">Parent</label>
-                            <input type="checkbox" id="parent">
+                            <input type="checkbox" id="parent" name = "vol_parent">
                         </div>
                         <div class="input-field">
                             <label for="counselor">Merit Badge Counselor</label>
-                            <input type="checkbox" id="counselor">
+                            <input type="checkbox" id="counselor" name = "vol_counselor">
                         </div>
                     </div>
                     <button class="submitButton">
